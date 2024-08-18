c472da1a585944de297399df70305d02ad270bec
mehrad roham <155972727+mehradroham@users.noreply.github.com>
2024-08-18 13:26:14 +0330

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 
 const app=express()
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : process.env.FRONTEND_URL,
     credentials : true
 }))
 app.use(express.json())
